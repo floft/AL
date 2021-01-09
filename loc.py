2f59ac3091d0981a2de2862b8ccaff4df7e2f245
Bryan Minor <bminor@wsu.edu>
2021-01-08 17:41:11 -0800

    Fixed typo.

--- loc.py
+++ loc.py
@@ -301,7 +301,7 @@ class Location:
         v1 = utils.clean(float(v1), -1.0, 1.0)
         self.accz.append(v1)
         temp += v1 * v1
-        self.acctotal.append(np.sqrt(temp))  # compute combined accuracy
+        self.acctotal.append(np.sqrt(temp))  # compute combined acceleration
 
         valid, date, sen_time, f1, f2, v1, v2 = self.read_entry(infile)
         self.latitude.append(float(v1))
