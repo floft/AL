79311dfd7d033ae10989d74e9f34ae504a34c4a6
Bryan Minor <bminor@wsu.edu>
2021-04-28 17:22:40 -0700

    Override the process_activity_label() method of AL so that we don't translate activity labels.

--- multioc.py
+++ multioc.py
@@ -15,17 +15,48 @@ value to the vector, then passed through the multi-class classifier.
 
 (Note that only train, test, and loo modes are currently supported.)
 """
+from datetime import datetime
+from typing import Dict, Union, Optional
+
+import al
 import config
-from al import AL
 
 
-class MultiOC(AL):
+class MultiOC(al.AL):
     """
     Main class to run "multi one-class" activity modeling. Inherits from AL so that we can use most
     of its functions.
     """
 
-    pass
+    def process_activity_label(self, event: Dict[str, Union[datetime, float, str, None]]) \
+            -> Optional[str]:
+        """
+        Override the original label processing to just convert spaces to underscores. Don't do
+        translations.
+
+        This will result in the `ydata` labels of the feature extraction being original activity
+        names.
+
+        Parameters
+        ----------
+        event : Dict[str, Union[datetime, float, str, None]]
+            An event dictionary with fields mapped to their respective values
+            Normally these would be output from the CSV Data Layer
+
+        Returns
+        -------
+        Optional[str]
+            The cleaned but not translated event name, or None if the original was None
+        """
+
+        original_label = event[self.conf.label_field_name]
+
+        # Return None if the original label is None:
+        if original_label is None:
+            return None
+
+        # Replace spaces with underscores:
+        return original_label.replace(' ', '_')
 
 
 if __name__ == '__main__':
@@ -52,7 +83,8 @@ if __name__ == '__main__':
     moc.location.read_locations()
 
     if cf.mode in [config.MODE_TRAIN_MODEL, config.MODE_TEST_MODEL, config.MODE_LEAVE_ONE_OUT]:
-        pass
+        # We need feature vectors and labels for these modes, so extract the features:
+        data_by_file = al.gather_features_by_file(files=cf.files, al=moc)
     else:
         msg = f"Mode {cf.mode} is not supported by this script."
         raise ValueError(msg)
