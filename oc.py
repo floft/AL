4df055930f59dba502d2f288dd137b94d156aaf1
Bryan Minor <bminor@wsu.edu>
2021-05-11 13:58:19 -0700

    Started on new oc.py script.

---
+++ oc.py
@@ -0,0 +1,9 @@
+"""
+Code for training and using one-class activity models, which predict a single class at a time. Based
+on the code for multi-class model in `al.py`.
+
+Written by Diane J. Cook and Bryan Minor, Washington State University
+
+Copyright (c) 2021. Washington State University (WSU). All rights reserved.
+Code and data may not be used or distributed without permission from WSU.
+"""
